5a3999cc345f576ba8c05b4ddb225db040823f76
KariinDeLeeuw <65500409+KariinDeLeeuw@users.noreply.github.com>
2021-09-21 19:45:09 +0200

    Added all keycodes in an array

--- script.js
+++ script.js
@@ -95,3 +95,70 @@ var allSounds = [
     icon: "img/cow.svg"
   }
 ];
+
+//Hier komen alle keycodes gekoppeld aan een drumpad in een array
+var drumpadSounds = [
+  {
+      key:"103",
+      naam:allSounds[0].naam,
+      soundLink: allSounds[0].soundLink,
+      color:allSounds[0].color,
+      icon: allSounds[0].icon
+  },
+  {
+      key:"104",
+      naam:allSounds[1].naam,
+      soundLink: allSounds[1].soundLink,
+      color:allSounds[1].color,
+      icon: allSounds[1].icon
+  },
+  {
+      key:"105",
+      naam:allSounds[2].naam,
+      soundLink: allSounds[2].soundLink,
+      color:allSounds[2].color,
+      icon: allSounds[2].icon
+  },
+  {
+      key:"100",
+      naam:allSounds[3].naam,
+      soundLink: allSounds[3].soundLink,
+      color:allSounds[3].color,
+      icon: allSounds[3].icon
+  },
+  {
+      key:"101",
+      naam:allSounds[4].naam,
+      soundLink: allSounds[4].soundLink,
+      color:allSounds[4].color,
+      icon: allSounds[4].icon
+  },
+  {
+      key:"102",
+      naam:allSounds[5].naam,
+      soundLink: allSounds[5].soundLink,
+      color:allSounds[5].color,
+      icon: allSounds[5].icon
+  },
+  {
+      key:"97",
+      naam:allSounds[6].naam,
+      soundLink: allSounds[6].soundLink,
+      color:allSounds[6].color,
+      icon: allSounds[6].icon
+  },
+  {
+      key:"98",
+      naam:allSounds[7].naam,
+      soundLink: allSounds[7].soundLink,
+      color:allSounds[7].color,
+      icon: allSounds[7].icon
+  },
+  {
+      key:"99",
+      naam:allSounds[8].naam,
+      soundLink: allSounds[8].soundLink,
+      color:allSounds[8].color,
+      icon: allSounds[8].icon
+  }
+]
